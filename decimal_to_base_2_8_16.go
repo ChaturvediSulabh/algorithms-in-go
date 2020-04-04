8570a7b4907a5cce168e7227c4012321fb09eee8
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-04-04 16:10:26 +0100

    convert a decimal to any base

---
+++ decimal_to_base_2_8_16.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(decimalToBase(10, 2))
+	fmt.Println(decimalToBase(10, 8))
+	fmt.Println(decimalToBase(10, 16))
+}
+
+func decimalToBase(num, base int) string {
+	switch base {
+	case 2:
+		return fmt.Sprintf("%b", num)
+	case 8:
+		return fmt.Sprintf("%o", num)
+	case 16:
+		return fmt.Sprintf("%X", num)
+	}
+	return ""
+}
